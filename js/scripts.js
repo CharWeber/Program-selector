9c362875d8a7e7f2e8312670ba271cd199f24c10
Charlie Weber <charlestweber@gmail.com>
2021-08-13 10:19:43 -0700

    add js branching logic tree for langtest form

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,43 @@ $(document).ready(function(){
     event.preventDefault();
     const rain = $("select#rain").val();
     const cof = $("input:radio[name=coffee]:checked").val();
-    const speed = document.getElementById("fast").val();
+    const speed = parseInt(document.getElementById("fast").value);
     const snake = $("input:radio[name=snake]:checked").val();
     const cheese = $("select#cheese").val();
+    $("#langMatch").show();
 
+    if (snake === yesSnake){
+      $("#langName").text("Python!")
+    }
+    else if (cheese === sharpCheese){
+      $("#langName").text("C#!")
+    }
+    else if (speed >= 50){
+      if (cof === water){
+        if (rain === Y || rain === both){
+          $("#langName").text("Rust!")
+        }
+        else{
+          $("#langName").text("Ruby!")
+        }
+      }
+      else (cof === dark || cof === cream) {
+        if (rain === N) {
+          $("#langName").text("Javascript!")
+        }
+        else {
+          $("#langName").text("GO!")
+        }
+      }
+    }
+    else (speed < 50) {
+      if (rain === Y || rain === both){
+        $("#langName").text("Ruby")
+      }
+      else {
+        $("#langName").text("Swift")
+      }
+    }
 
 
   });
